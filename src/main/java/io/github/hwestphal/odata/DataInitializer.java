4740d4fff641e82e2d8490b7aa536db89eb271a9
Kenny Aguirre <jken1102@gmail.com>
2018-01-18 18:56:53 -0500

    2

--- src/main/java/io/github/hwestphal/odata/DataInitializer.java
+++ src/main/java/io/github/hwestphal/odata/DataInitializer.java
@@ -24,7 +24,7 @@ public class DataInitializer implements ApplicationRunner {
     @Override
     public void run(ApplicationArguments args) throws Exception {
 	Customer customer = new Customer("C1");
-	customer.setName("Customer 1");
+	customer.setName("Cliente BCP");
 	
 	
 	
